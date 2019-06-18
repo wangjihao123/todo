daef1c0ef1a5cf486b07bd6544c61b18d360d335
wjh <wjh.qq.com>
2019-06-18 10:13:38 +0800

    补全链接

---
+++ todolist/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request,'home.html')
+
+def edit(request):
+    return render(request,'edit.html')
+
+def about(request):
+    return render(request,'about.html')
